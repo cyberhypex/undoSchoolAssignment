7e86ddc1c66eec05c9861fe117c06af4ce958d06
Anshuman <chaoanxuman999@gmail.com>
2025-07-16 00:20:28 +0530

    All methods done and tested

--- src/main/java/com/undoSchool/demo/Controllers/CourseController.java
+++ src/main/java/com/undoSchool/demo/Controllers/CourseController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
 import java.util.List;
 
 @RestController
@@ -22,5 +23,35 @@ public class CourseController {
     public List<CourseDocument> searchWithKeyWord(@RequestParam String keyword){
         return courseService.searchCoursesWithKeyWord(keyword);
     }
+
+    @GetMapping("/condition")
+    public List<CourseDocument> searchByMinAndMaxAge(@RequestParam Integer minAge,
+                                                     @RequestParam Integer maxAge
+                                                     ){
+        return courseService.searchCriteria(minAge,maxAge);
+    }
+    @GetMapping("/price")
+    public List<CourseDocument> searchByPriceRange(@RequestParam Double minPrice,
+                                                   @RequestParam Double maxPrice) {
+        return courseService.searchPrice(minPrice, maxPrice);
+    }
+
+    @GetMapping("/category")
+    public List<CourseDocument> searchCategory(@RequestParam String category){
+        return courseService.searchExact(category);
+    }
+    @GetMapping("/category/type")
+    public List<CourseDocument> searchCategoryType(@RequestParam String type){
+        return courseService.searchExactType(type);
+    }
+    @GetMapping("/search/date")
+    public List<CourseDocument> searchCoursesFromDate(@RequestParam String date) {
+        Instant fromDate = Instant.parse(date.trim());
+
+        return courseService.findCoursesOnOrAfterDate(fromDate);
+    }
+
+
+
 
 }
--- src/main/java/com/undoSchool/demo/Service/CourseService.java
+++ src/main/java/com/undoSchool/demo/Service/CourseService.java
@@ -2,15 +2,20 @@ package com.undoSchool.demo.Service;
 
 
 import com.undoSchool.demo.Model.CourseDocument;
-import com.undoSchool.demo.Repositories.CourseRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.client.elc.NativeQuery;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.query.Criteria;
+
+import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
+import org.springframework.data.elasticsearch.core.query.Query;
 import org.springframework.stereotype.Service;
 
-import javax.management.Query;
+import java.nio.charset.CoderResult;
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,6 +37,60 @@ public class CourseService {
                 SearchHit::getContent
         ).collect(Collectors.toList());
     }
+
+
+    public List<CourseDocument> searchCriteria(Integer minAge,Integer maxAge){
+        Criteria criteria=new Criteria("minAge").is(minAge).and("maxAge").is(maxAge);
+        Query query = new CriteriaQuery(criteria);
+
+        SearchHits<CourseDocument> hits=elasticsearchOperations.search(query,CourseDocument.class);
+        return hits.stream().map(
+                SearchHit::getContent
+        ).collect(Collectors.toList());
+
+    }
+
+    public List<CourseDocument> searchPrice(Double minPrice, Double maxPrice) {
+        Criteria criteria = new Criteria("price")
+                .greaterThanEqual(minPrice)
+                .and("price").lessThanEqual(maxPrice);
+
+        Query query = new CriteriaQuery(criteria);
+        SearchHits<CourseDocument> hits = elasticsearchOperations.search(query, CourseDocument.class);
+
+        return hits.stream()
+                .map(SearchHit::getContent)
+                .collect(Collectors.toList());
+    }
+
+    public List<CourseDocument> searchExact(String category){
+        Criteria criteria=new Criteria("category").is(category);
+        Query query=new CriteriaQuery(criteria);
+        SearchHits<CourseDocument> hits=elasticsearchOperations.search(query,CourseDocument.class);
+        return hits.stream().map(
+                SearchHit::getContent
+        ).collect(Collectors.toList());
+    }
+    public List<CourseDocument> searchExactType(String type){
+        Criteria criteria=new Criteria("type").is(type);
+        Query query=new CriteriaQuery(criteria);
+        SearchHits<CourseDocument> hits=elasticsearchOperations.search(query,CourseDocument.class);
+        return hits.stream().map(
+                SearchHit::getContent
+        ).collect(Collectors.toList());
+    }
+
+    public List<CourseDocument> findCoursesOnOrAfterDate(Instant fromDate) {
+        Criteria criteria = new Criteria("nextSessionDate").greaterThanEqual(fromDate);
+        CriteriaQuery query = new CriteriaQuery(criteria);
+
+        SearchHits<CourseDocument> hits = elasticsearchOperations.search(query, CourseDocument.class);
+
+        return hits.stream()
+                .map(SearchHit::getContent)
+                .collect(Collectors.toList());
+    }
+
 
 
 }
